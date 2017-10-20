9e6b30184d09e2bb60bbb4e53e5b785e87282bb9
Nouroz Rahman <nouroz_rahman@yahoo.com>
2017-10-20 12:00:12 +0600

    Update image_processing_2.py

--- image_processing_2.py
+++ image_processing_2.py
@@ -7,26 +7,31 @@ from scipy import misc
 # IMPORT IMAGE
 a = scipy.misc.imread("royal_tiger.jpeg", flatten = True)
 
+# FUNCTION TO CONVERT NDARRY TO IMAGE
 def image_show(image):
 
 	image = Image.fromarray(np.uint8(image))
 	return image.show()
 
+# FUNCTION TO ADD RANDOM NOISE
 def add_noise(image):
 
 	noise =  image.std() * np.random.random(image.shape)
 	noisy_image = image + noise 
 	return noisy_image
 
+# FUNCTION FOR DENOISING NOISY IMAGE USING GAUSSIAN FILTER
 def denoise_image(image, sigma):
 
 	denoised_image = scipy.ndimage.gaussian_filter(image, sigma)
 	return denoised_image
 
+# FUNCTION FOR DENOISING NOISY IMAGE USING MEDIAN FILTER
 def denoise_with_median_filter(image, param):
 
 	denoise_median = scipy.ndimage.median_filter(image, param)
 	return denoise_median
+
 
 def run():
 	noisy_image = add_noise(a)
@@ -36,6 +41,7 @@ def run():
 	image_show(noisy_image)
 	image_show(denoised_image)
 	image_show(denoise_median)
+
 
 	
 if __name__ == '__main__':
